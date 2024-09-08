031d8669d5b1ffdb597124c034a9555c57206165
Abiud Kiprotich <abiud.kiprotich@student.moringaschool.com>
2024-09-09 01:19:16 +0300

    added functions to coffee.py

--- coffee.py
+++ coffee.py
@@ -1,15 +1,36 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
+        self._validate_name()
+        self._orders = []
+    
+    @property
+    def name(self):
+        return self._name
+
+    def _validate_name(self):
+        if not isinstance(self._name, str) or len(self._name) < 3:
+            raise ValueError("Name must be a string with at least 3 characters.")
+
         
     def orders(self):
-        pass
+         return self._orders
+
     
     def customers(self):
-        pass
+         return list({order.customer for order in self._orders})
     
     def num_orders(self):
-        pass
+          return len(self._orders)
+
     
     def average_price(self):
-        pass
+        if not self._orders:
+            return 0
+        total_price = sum(order.price for order in self._orders)
+        return total_price / len(self._orders)
+
+
+coffee1 = Coffee("Latte")
+
+print(coffee1) 
